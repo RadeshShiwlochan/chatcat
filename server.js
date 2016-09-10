ca9e7d21ef1e15e0d0b1ce6f593d8f91a3beb653
radeshshiwlochan <radesh0430@gmail.com>
2016-09-10 18:00:43 -0400

    adds a dashboard route and a port function

--- server.js
+++ server.js
@@ -2,11 +2,16 @@
 
 const express = require('express');
 const app     = express();
+app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res, next) => {
 	res.send('<h1>Hello Express!</h1>');
 });
 
-app.listen(3000, () => {
-	console.log('ChatCat Running on Port 3000');
-})
+app.get('/dashboard', (req, res, next) => {
+	res.send('<h1>This is the DashBoard!!');
+});
+
+app.listen(app.get('port'), () => {
+	console.log('ChatCat Running on Port ', app.get('port'));
+});
