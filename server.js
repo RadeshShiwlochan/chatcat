d9ad9cee4efb118bb723b2f05e6ec30d2a0339b5
radeshshiwlochan <radesh0430@gmail.com>
2016-09-10 17:53:21 -0400

    adds a route for home page

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log("Welcome to ChatCat!!");
+const express = require('express');
+const app     = express();
+
+app.get('/', (req, res, next) => {
+	res.send('<h1>Hello Express!</h1>');
+});
+
+app.listen(3000, () => {
+	console.log('ChatCat Running on Port 3000');
+})
