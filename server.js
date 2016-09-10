ee02fb994089d778685c2fbafaa3f885a708c7cb
radeshshiwlochan <radesh0430@gmail.com>
2016-09-10 18:30:41 -0400

    adds a custom middleware, for demo purposes

--- server.js
+++ server.js
@@ -2,10 +2,18 @@
 
 const express = require('express');
 const app     = express();
+
+let helloMiddleware = (req, res, next) => {
+	req.hello = "Hello!, this is the helloMiddleware! ";
+	next();
+}
+
+app.use(helloMiddleware);
+
 app.set('port', process.env.PORT || 3000);
 
 app.get('/', (req, res, next) => {
-	res.send('<h1>Hello Express!</h1>');
+	res.send('<h1>Hello Express!</h1>' + req.hello);
 });
 
 app.get('/dashboard', (req, res, next) => {
