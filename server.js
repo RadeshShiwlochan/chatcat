d8b070c9e1db7c925cbe3c98410b2ba723f69427
radeshshiwlochan <radesh0430@gmail.com>
2016-09-11 11:49:13 -0400

    includes the app folder as a module to use the routes

--- server.js
+++ server.js
@@ -2,11 +2,13 @@
 
 const express = require('express');
 const app     = express();
+const chatCat = require('./app');
 
 app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
-
 app.set('view engine', 'ejs');
+
+app.use('/', chatCat.router);
 
 
 app.get('/dashboard', (req, res, next) => {
