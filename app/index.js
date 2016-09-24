bf45d4017ba74c080075a15cc4e4f4f168f9de9a
radeshshiwlochan <radesh0430@gmail.com>
2016-09-23 23:30:51 -0400

    sets session variable along with secret key

--- app/index.js
+++ app/index.js
@@ -1,18 +1,27 @@
 'use strict';
 const router = require('express').Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', (req, res, next) =>{
 	res.render('login', {
 		pageTitle: 'My Login Page'
 	});
 });
 
-router.get('/chatroom', (req,res,next)=> {
+router.get('/chatroom', (req,res,next) =>{
 	res.render('chatroom');
 });
 
-router.get('/rooms', (req,res, next)=> {
+router.get('/rooms', (req,res, next) =>{
 	res.render('rooms');
+});
+
+router.get('/getsession', (req, res, next) =>{
+	res.send("My Favorite Color " + req.session.favColor);
+});
+
+router.get('/setsession', (req,res,next) =>{
+	req.session.favColor = "Red";
+	res.send("Session Set");
 });
 
 router.use((req,res,next) => {
