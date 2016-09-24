5c70436c53131542eec23fe7e05c8089ede90342
radeshshiwlochan <radesh0430@gmail.com>
2016-09-23 22:55:41 -0400

    exports Mongoose Object

--- app/db/index.js
+++ app/db/index.js
@@ -5,3 +5,7 @@ const Mongoose = require('mongoose').connect(config.dbURI);
 Mongoose.connection.on('error', error => {
 	console.log("MongoDB Error: ", error);
 });
+
+module.exports = {
+	Mongoose
+}
