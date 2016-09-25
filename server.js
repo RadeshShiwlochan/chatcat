ff79a43b9d3ed80a2bcb1cdadeb9e2a46802a2d8
radeshshiwlochan <radesh0430@gmail.com>
2016-09-24 23:24:59 -0400

    adds passport module, adds facebook variables

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.set('port', process.env.PORT || 3000);
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+app.use('/getsession', chatCat.session);
+
 app.use('/', chatCat.router);
 
 
