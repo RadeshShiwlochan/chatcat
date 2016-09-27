ddd00e6d22a72cb1dc90ddae4c6511495437c402
radeshshiwlochan <radesh0430@gmail.com>
2016-09-26 22:49:05 -0400

    changes the callback definition to outside the function

--- app/auth/index.js
+++ app/auth/index.js
@@ -3,6 +3,12 @@ const passport = require('passort');
 const config = require('../config');
 const Facebook Strategy = require('passport-facebook').Strategy;
 
+let authProcessor = (accessToken, refreshToken, profile, done) => {
+		//Find a user in the local db using profile.id
+		//if the user is found, return the user data using the done()
+		//if the use is not found, create one in the local db and return
+	}
+
 module.exports = () => {
-	passport.use(new FacebookStrategy());
+	passport.use(new FacebookStrategy(config.fb, authProcessor ));
 }
