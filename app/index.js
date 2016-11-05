216749db07afb16d1421bffcdef7a31cf82cc106
radeshshiwlochan <radesh0430@gmail.com>
2016-11-04 20:02:32 -0400

    fixes some errors in server.js

--- app/index.js
+++ app/index.js
@@ -1,6 +1,7 @@
 'use strict';
 const router = require('express').Router();
 const db = require('../app/db')
+require('./auth')();
 
 router.get('/', (req, res, next) =>{
 	res.render('login', {
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 'use strict';
 
-const express = require('express');
-const app     = express();
-const chatCat = require('./app');
+const express  = require('express');
+const app      = express();
+const chatCat  = require('./app');
 const passport = require('passport');
 
 app.set('port', process.env.PORT || 3000);
@@ -12,10 +12,18 @@ app.set('view engine', 'ejs');
 //app.use('/getsession', chatCat.session);
 
 app.use('/', chatCat.router);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get('/dashboard', (req, res, next) => {
-	res.send('<h1>This is the DashBoard!!');
+	res.send('<h1>This is the DashBoard!!</h1>');
 });
+
+/*
+app.get('/', (req, res, next) => {
+	res.send('<h1>Hello ChatCat!</h1>');
+});
+*/
 
 app.get('/auth/facebook':passport.authenticate('facebook'),
 	'/auth/facebook/callback': passport.authenticate('facebook', {
