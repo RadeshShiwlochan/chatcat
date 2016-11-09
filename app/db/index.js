41feb4f52b077d4c6c5d4e3d2fc772299a6c1e62
radeshshiwlochan <radesh0430@gmail.com>
2016-11-08 23:16:42 -0500

    makes some changes to db injex file

--- app/db/index.js
+++ app/db/index.js
@@ -2,6 +2,7 @@
 const config = require('../config');
 const Mongoose = require('mongoose').connect(config.dbURI);
 
+//Log an error if the connection fails
 Mongoose.connection.on('error', error => {
 	console.log("MongoDB Error: ", error);
 });
